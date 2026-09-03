2714cbec7eede8c4dcd0d7827ff8ce45b36c171e
Max <max.gambichler.dev@gmail.com>
2026-09-02 22:59:26 -0400

    SPRINT 1 REQUIRMENTS COMPLETE
    Buttons now swap between credits, notes, and back to main menu
    
    music and sound effects have been added

--- group-project---imgd-1001/scripts/main_menu.gd
+++ group-project---imgd-1001/scripts/main_menu.gd
@@ -1,19 +1,23 @@
 extends Control
 
-@onready var grid_container: GridContainer = $TemporaryName/GridContainer
+@onready var grid_container: GridContainer = $buttonsContainer
+@onready var credits: PanelContainer = $credits
+@onready var version_notes: PanelContainer = $versionNotes
+
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	for button in grid_container.get_children():
 		if button is Button:
-			
 			button.mouse_entered.connect(_on_mouse_entered.bind(button))
 			button.mouse_exited.connect(_on_mouse_exited.bind(button))
-			
 
 func _on_mouse_entered(chosen_button: Button) -> void:
 	var texture = chosen_button.get_node("TextureRect")
+	audio_stream_player_2d.play()
 	texture.modulate = Color(0.8,0.8,0.8) # Replace with function body.
+	
 
 func _on_mouse_exited(chosen_button: Button) -> void:
 	var texture = chosen_button.get_node("TextureRect")
@@ -25,11 +29,27 @@ func _on_button_pressed_Start() -> void:
 
 
 func _on_button_2_pressed_Version_Notes() -> void:
-	pass # Replace with function body.
+	grid_container.visible = false
+	version_notes.visible = true
+	
 
 func _on_button_3_pressed_Credits() -> void:
-	pass # Replace with function body.
+	grid_container.visible = false
+	credits.visible = true
 
 
 func _on_button_4_pressed_Exit() -> void:
 	get_tree().quit()
+
+
+func _on_button_pressed_Back() -> void:
+	if credits.visible == true:
+		credits.visible = false
+		grid_container.visible = true
+	else:
+		version_notes.visible = false
+		grid_container.visible = true
+
+
+func _on_button_mouse_entered_Back() -> void:
+	audio_stream_player_2d.play()
