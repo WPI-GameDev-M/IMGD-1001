94db32a3ba5cfa6bba41c2e947415c5f6dd5812e
AverageDreadmaw2 <ymoonshine@wpi.edu>
2026-09-22 10:49:31 -0400

    added spacebar to skip intro animation and also more character pngs for testing with
    character pngs not used yet but are there for testing soon

--- group-project---imgd-1001/scripts/game/boot_splash.gd
+++ group-project---imgd-1001/scripts/game/boot_splash.gd
@@ -7,3 +7,9 @@ extends Control
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	get_tree().change_scene_to_packed(main_menu)
 	queue_free()
+	
+# Click spacebar to skip animation
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("Jump"):
+			get_tree().change_scene_to_packed(main_menu)
+			queue_free()
